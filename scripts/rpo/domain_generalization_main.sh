31d68e7d1fb41a00b33f5e435ef37ff6e9594164
dongdori <dongdori>
2023-06-01 19:03:45 +0900

    fix readme and configs

--- scripts/rpo/domain_generalization_main.sh
+++ scripts/rpo/domain_generalization_main.sh
@@ -3,7 +3,7 @@ EPOCH=10
 
 for seed in 0 1 2
 do
-    sh scripts/rpo/xd_train.sh imagenet ${seed} ${GPU} imagenet_k4_ep10
+    sh scripts/rpo/xd_train.sh imagenet ${seed} ${GPU} imagenet_k24_ep15
     for dataset in imagenet imagenet_a imagenet_r imagenet_sketch imagenetv2
     do
         sh scripts/rpo/xd_test.sh ${dataset} ${seed} ${EPOCH} ${GPU}
